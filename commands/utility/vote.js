c681341c62cd9dee78eb3a756e234f06404e2c18
Sébastien <sbages.dev@gmail.com>
2021-06-20 03:21:21 +0200

    work in progress

--- commands/utility/vote.js
+++ commands/utility/vote.js
@@ -14,29 +14,39 @@ module.exports = {
 	execute() {
 
 		const webhook = new WebhookClient(webhooks.votesKeeper.id, webhooks.votesKeeper.token);
-		const url = 'https://api.top-serveurs.net/v1/servers/' + tokenTopServer;
 
-		axios.get(url)
-			.then(function(response) {
+		const getUrlTopServeur = async () => {
+			const response = await axios.get('https://api.top-serveurs.net/v1/servers/' + tokenTopServer);
+			return response.data.server.slug;
+		};
 
-				const { server } = response.data;
-				const topServeurUrl = 'https://top-serveurs.net/conan-exiles/vote/' + server.slug;
+		const getNumberOfVotes = async () => {
+			const response = await axios.get('https://api.top-serveurs.net/v1/servers/' + tokenTopServer + '/stats');
+			const date = new Date().getMonth();
+			const months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december' ];
+			const currentMonth = months[date];
+			return response.data.stats.monthly[0][currentMonth + '_votes'];
+		};
 
-				const messageEmbed = new MessageEmbed()
-					.setColor(color)
-					.setTitle('VOTEZ POUR LE SERVEUR')
-					.setURL(topServeurUrl)
-					.attachFiles(['./images/topServeur.png'])
-					.setThumbnail('attachment://topServeur.png')
-					.setDescription('N\'hésitez pas à donner un coup de pouce au serveur en votant. Merci pour votre participation :thumbsup:')
-					.addField('LIEN TOP SERVEUR', topServeurUrl);
+		const getData = async () => {
+			const serverSlug = await getUrlTopServeur();
+			const numberOfVotes = await getNumberOfVotes();
+			const topServeurUrl = 'https://top-serveurs.net/conan-exiles/vote/' + serverSlug;
 
-				return webhook.send(messageEmbed)
-					.catch(console.error);
-			})
-			.catch(function(error) {
-				console.log(error);
-			});
+			const messageEmbed = new MessageEmbed()
+				.setColor(color)
+				.setTitle('VOTEZ POUR LE SERVEUR')
+				.setURL(topServeurUrl)
+				.attachFiles(['./images/topServeur.png'])
+				.setThumbnail('attachment://topServeur.png')
+				.setDescription('N\'hésitez pas à donner un coup de pouce au serveur en votant. Merci pour votre participation :thumbsup:')
+				.addField('LIEN TOP SERVEUR', topServeurUrl)
+				.setFooter(`Pour l'instant, nous comptabilisons ${numberOfVotes} votes ce mois-ci`);
 
+			return webhook.send(messageEmbed)
+				.catch(console.error);
+		};
+
+		getData();
 	},
 };
